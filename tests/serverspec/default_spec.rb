a7ba990aa7c13cdb733ba4e920ea9e971f5fc4c6
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-19 21:55:13 +0900

    add more tests

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -5,6 +5,10 @@ config = "/etc/installurl"
 url = "https://fastly.cdn.openbsd.org/pub/OpenBSD"
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by "root" }
+  it { should grouped_into "wheel" }
   its(:content) { should match Regexp.escape(url) }
 end
